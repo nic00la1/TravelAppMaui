31d5cf0c15a7b0371102d51325af65239e08bf37
nic00la1 <nic00la.kaleta@gmail.com>
2024-02-09 18:30:18 +0100

    Added bell 🔔🔔

--- TravelAppMaui/MauiProgram.cs
+++ TravelAppMaui/MauiProgram.cs
@@ -15,6 +15,13 @@ namespace TravelAppMaui
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("boogaloo.regular.ttf", "Regular");
+
+                    // Icons for the project
+                    fonts.AddFont("Brands-Regular-400.otf", "FAB");
+                    fonts.AddFont("Free-Regular-400.otf", "FAR");
+                    fonts.AddFont("Free-Solid-900.otf", "FAS");
+
+
                 });
             builder.Services.AddSingleton<MainPageViewModel>();
             builder.Services.AddSingleton<MainPage>();
