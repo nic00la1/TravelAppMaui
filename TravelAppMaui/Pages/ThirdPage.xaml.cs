e7c4b99fbf11722338814595cfde48d4d6d4fbcb
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-13 13:57:34 +0100

    logika przycisku ✅

--- TravelAppMaui/Pages/ThirdPage.xaml.cs
+++ TravelAppMaui/Pages/ThirdPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class ThirdPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Second");
+    }
 }
